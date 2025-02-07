1f61cc85bce06b6e48dcd2b320817d71dea2f095
husejn09 <husejn138@outlook.com>
2025-02-07 08:22:25 +0100

    Config files setup

---
+++ reactNativeApp/app/styles.config.js
@@ -0,0 +1,8 @@
+import "../global.css"
+export const formStyles = {
+    text: "mb-2 text-base text-gray-100 font-medium",
+    main: "flex-row border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-orange items-center",
+  };
+
+
+  export default formStyles
