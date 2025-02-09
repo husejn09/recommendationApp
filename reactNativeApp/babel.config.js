6037f456a6d9b0d5bb9afc69f5efc5952e1e0dcc
husejn09 <husejn138@outlook.com>
2025-02-09 20:09:49 +0100

    Changes for build on EAS

--- reactNativeApp/babel.config.js
+++ reactNativeApp/babel.config.js
@@ -9,8 +9,7 @@ module.exports = function (api) {
         ['module:react-native-dotenv', {
           moduleName: '@env',
           path: '.env',
-        }],
-        ["transform-remove-console", { "exclude": ["error", "warn"] }],
+        }]
       ],
     };
   };
